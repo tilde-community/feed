758828b106be8d411e395ffea578380d309f9e73
Arnelle Balane <arnellebalane@gmail.com>
2016-03-12 02:03:42 +0800

    ping api endpoint for feeds

--- source/static/javascripts/main.js
+++ source/static/javascripts/main.js
@@ -1,11 +1,27 @@
 require(['jquery', 'components/feeds'], ($, _feeds) => {
     let feeds = _feeds($('.feeds-list'));
 
-    setInterval(() => {
-        feeds.add({
-            body: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
-            kind: ['world-enter', 'world-exit', 'player-level-up', 'player-finished', 'monster-found',
-                'monster-search', 'monster-attack'][Math.floor(Math.random() * 7)]
+    const ENDPOINT = 'http://localhost:8000/activities';
+    let lastFeed = 0;
+
+
+    function fetchFeeds(callback) {
+        $.ajax({
+            url: ENDPOINT,
+            type: 'GET',
+            data: { last: lastFeed },
+            success: (data) => {
+                data = JSON.parse(data);
+                lastFeed = data[data.length - 1].pk;
+                callback(data);
+            }
         });
-    }, 1000);
+    }
+
+
+    setInterval(() => {
+        fetchFeeds((data) => {
+            data.forEach(feeds.add);
+        });
+    }, 100);
 });
