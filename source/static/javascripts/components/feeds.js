b62e62ed4d00e017fbddd73cbfa81a8ede8f320c
Arnelle Balane <arnellebalane@gmail.com>
2016-03-12 01:50:53 +0800

    script to handle list scrolling

--- source/static/javascripts/components/feeds.js
+++ source/static/javascripts/components/feeds.js
@@ -3,11 +3,18 @@ define(['jquery', 'mustache'], ($, Mustache) => {
 
 
     function add($feeds, feed) {
+        let height = $feeds[0].scrollHeight;
         let $feed = Mustache.render(TEMPLATE, feed);
         $feeds.prepend($feed);
+
         if ($feeds.scrollTop() < 100) {
             $feeds.scrollTop(0);
+        } else {
+            let difference = $feeds[0].scrollHeight - height;
+            let scrollTop = $feeds.scrollTop() + difference;
+            $feeds.scrollTop(scrollTop);
         }
+
         return $feed;
     }
 
